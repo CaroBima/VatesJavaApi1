7f91311382cac4aece8e72936c45470e5d3fda14
Caro Bima <carobima@gmail.com>
2023-06-26 10:59:54 -0300

    Agregando ClimaEntity y data.sql para probar la persistencia con h2

---
+++ src/main/java/com/digitalworlds/api1/entities/ClimaEntity.java
@@ -0,0 +1,42 @@
+package com.digitalworlds.api1.entities;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Getter
+@Setter
+@RequiredArgsConstructor
+@Entity (name="clima")
+public class ClimaEntity {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String ciudad;
+
+    private String pais;
+
+    @Column(name = "tempcelsius")
+    private Double tempCelsius;
+
+    @Column(name = "ultimaactualizacion")
+    private String ultimaActualizacion;
+
+    private int humedad;
+
+    @Column(name = "sensaciontermica")
+    private Double sensacionTermica;
+
+    @Column(name = "vientokmxh")
+    private Double vientoKmxH;
+
+    @Column(name = "fechaconsulta")
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date fechaConsulta;
+}
