b8e6514da416d265e305f512a140d6d7352a7961
CaroBima <carobima@gmail.com>
2023-06-30 21:19:50 -0300

    Agregada autenticacion basica con usuario y clave

--- src/main/java/com/digitalworlds/api1/configuration/SecurityConfig.java
+++ src/main/java/com/digitalworlds/api1/configuration/SecurityConfig.java
@@ -1,4 +1,36 @@
 package com.digitalworlds.api1.configuration;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
 public class SecurityConfig {
+    @Bean
+    public UserDetailsService userDetailsService(){
+        PasswordEncoder passwordEncoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+        return new InMemoryUserDetailsManager(
+                User.withUsername("admin")
+                        .password(passwordEncoder().encode("password"))
+                        .roles("ADMIN")
+                        .authorities("read", "write")
+                        .build(),
+
+                User.withUsername("user")
+                        .password(passwordEncoder.encode("password"))
+                        .roles("USER")
+                        .authorities("read")
+                        .build()
+                );
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
 }
